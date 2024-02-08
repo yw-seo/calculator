b8f2415a4733fbff670e471bf9f8f639c6db7505
[yw-seo] <yws4824@gmail.com>
2024-02-08 10:33:49 +0900

    git bash에서 시행착오로 보류중인 파일들

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # ch 4.2.1 main.py
 import sys 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox)
+from PyQt5.QtGui import QIcon # icon추가를 위한 라이브러리
+
 class Calculator(QWidget) :
 
     def __init__(self):
@@ -8,12 +10,26 @@ class Calculator(QWidget) :
         self.initUI()
 
     def initUI(self):
+        self.btn1=QPushButton('Message', self) #버튼 추가
+        self.btn1.clicked.connect(self.activateMessage) # 버튼 클릭시 핸들러 함수 연결
+
+        vbox=QVBoxLayout() #수직 레이아웃 위제 생성
+        vbox.addStretch(1) # 빈 공간
+        vbox.addWidget(self.btn1) #버튼 위치
+        vbox.addStretch(1) #빈 공간
+
+        self.setLayout(vbox) #빈 공간-버튼-빈공간 순으로 수직배치된 레이아웃 설정
+
+
         self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('icon.png')) # 왼도우 아이콘 추가
         self.resize(256,256)
         self.show()
 
+    def activateMessage(self): #버튼을 클릭할 때 동작하는 함수: 메시지 박스 출력
+        QMessageBox.information(self, "information", "Button clicked!")
+
 if __name__=='__main__':
-    app = QApplication(sys.argv)
-    view = Calculator()
+    app=QApplication(sys.argv)
+    view=Calculator()
     sys.exit(app.exec_())
-    
