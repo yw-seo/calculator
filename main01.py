1e518be58015a1acda30a82af7c70cdcc4d1e483
[yw-seo] <yws4824@gmail.com>
2024-02-08 09:26:09 +0900

    git bash에서 시행 착오 중 검토 요망

---
+++ main01.py
@@ -0,0 +1,19 @@
+# ch 4.2.1 main.py
+import sys 
+from PyQt5.QtWidgets import QApplication, QWidget
+class Calculator(QWidget) :
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Calculator')
+        self.resize(256,256)
+        self.show()
+
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    view = Calculator()
+    sys.exit(app.exec_())
+    
